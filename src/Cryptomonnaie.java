65d40555c51d91e3e0d3089500c39217c26e04e9
Thomas Glouche <gt203119-univ-lehavre.fr>
2021-05-26 14:41:24 +0200

    vrai Ajout de getters et setters

--- src/Cryptomonnaie.java
+++ src/Cryptomonnaie.java
@@ -5,7 +5,18 @@ public class Cryptomonnaie{
     public Cryptomonnaie(String nom, double valeurDeJeton){
         this.nom = nom;
         this.valeurDeJeton = valeurDeJeton;
+    }
 
-	//je modifie le fichier
+    public String getNom() {
+        return nom;
+    }
+
+    public double getValeurDeJeton() {
+        return valeurDeJeton;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%5s",nom) + ":" + String.format("%10.1f",valeurDeJeton);
     }
 }
